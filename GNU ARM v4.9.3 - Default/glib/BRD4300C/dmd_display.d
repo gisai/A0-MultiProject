659d31477f8294f0db547a27a438c128df3f573c
Diego Sanchez de Rivera <diego.sanchezderiveracordoba@gmail.com>
2017-03-15 16:55:03 +0100

    initial commit

---
+++ GNU ARM v4.9.3 - Default/glib/BRD4300C/dmd_display.d
@@ -0,0 +1,44 @@
+glib/BRD4300C/dmd_display.o: \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/platform/middleware/glib/dmd/display/dmd_display.c \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\display.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\emstatus.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\platform\middleware\glib\em_types.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\displayconfigall.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displaypalconfig.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayconfig.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/displayconfigapp.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/boards.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/ble-configuration.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/board_features.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\displayls013b7dh03.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\platform\middleware\glib\dmd\dmd.h
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\display.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\emstatus.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\platform\middleware\glib\em_types.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\displayconfigall.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displaypalconfig.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayconfig.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/displayconfigapp.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/boards.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/ble-configuration.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/board_features.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\displayls013b7dh03.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\platform\middleware\glib\dmd\dmd.h:
---
+++ GNU ARM v4.9.3 - Default/kit_lcd/BRD4300C/display.d
@@ -0,0 +1,47 @@
+kit_lcd/BRD4300C/display.o: \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/display.c \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/displayconfigall.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displaypalconfig.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayconfig.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/displayconfigapp.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/boards.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/ble-configuration.h \
+ C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/board_features.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\displayls013b7dh03.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\emstatus.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\platform\middleware\glib\em_types.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/display.h \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/emstatus.h \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/displaybackend.h
+
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/displayconfigall.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displaypalconfig.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayconfig.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/displayconfigapp.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/boards.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/ble-configuration.h:
+
+C:\Users\Diego\SimplicityStudio\v4_workspace\soc-smartPhone2/board_features.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\displayls013b7dh03.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\common\drivers\emstatus.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\platform\middleware\glib\em_types.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v1.0\hardware\kit\efr32bg1_brd4300c\config\displayls013b7dh03config.h:
+
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/display.h:
+
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/emstatus.h:
+
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.0/hardware/kit/common/drivers/displaybackend.h:
